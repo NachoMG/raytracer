06ac7cd9c5405c0b9f5c58372e794d647ffece13
Nacho <nmartinez@commercehub.com>
2024-06-21 22:45:20 +0200

    implemented Ray struct

---
+++ src/ray.rs
@@ -0,0 +1,16 @@
+use crate::vec3::Vector3;
+
+pub struct Ray {
+    origin: Vector3,
+    direction: Vector3,
+}
+
+impl Ray {
+    pub fn new(origin: Vector3, direction: Vector3) -> Ray {
+        Ray { origin, direction }
+    }
+
+    pub fn at(&self, t: f64) -> Vector3 {
+        self.origin + t * self.direction
+    }
+}
