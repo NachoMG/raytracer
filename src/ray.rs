6c88795f745d1f6fccc88532585b81ade52373f9
Nacho <nmartinez@commercehub.com>
2024-06-21 23:41:42 +0200

    shading with surface normals

--- src/ray.rs
+++ src/ray.rs
@@ -14,22 +14,29 @@ impl Ray {
         self.origin + t * self.direction
     }
 
-    pub fn hit_sphere(&self, center: Vector3, radius: f64) -> bool {
+    pub fn hit_sphere(&self, center: Vector3, radius: f64) -> f64 {
         let oc = center - self.origin;
         let a = self.direction.dot(self.direction);
         let b = -2.0 * self.direction.dot(oc);
         let c = oc.dot(oc) - radius * radius;
         let discriminant = b * b - 4.0 * a * c;
-        discriminant >= 0.0
+
+        if discriminant < 0.0 {
+            return -1.0;
+        } else {
+            return (-b - discriminant.sqrt()) / (2.0 * a);
+        }
     }
 
     pub fn color(&self) -> Vector3 {
-        if self.hit_sphere(Vector3::new(0.0, 0.0, -1.0), 0.5) {
-            return Vector3::new(1.0, 0.0, 0.0);
+        let t = self.hit_sphere(Vector3::new(0.0, 0.0, -1.0), 0.5);
+        if t > 0.0 {
+            let n = (self.at(t) - Vector3::new(0.0, 0.0, -1.0)).unit_vector();
+            return 0.5 * Vector3::new(n[0] + 1.0, n[1] + 1.0, n[2] + 1.0);
         }
 
         let unit_direction = self.direction.unit_vector();
         let a = 0.5 * (unit_direction[1] + 1.0);
-        (1.0 - a) * Vector3::new(1.0, 1.0, 1.0) + a * Vector3::new(0.5, 0.7, 1.0)
+        return (1.0 - a) * Vector3::new(1.0, 1.0, 1.0) + a * Vector3::new(0.5, 0.7, 1.0);
     }
 }
